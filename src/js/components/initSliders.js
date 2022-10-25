0b89c93e098d8d43c77c61651d1c419538322c34
Maksym <maksymRozhko7@gmail.com>
2022-10-25 21:34:34 +0300

    feat: shop

--- src/js/components/initSliders.js
+++ src/js/components/initSliders.js
@@ -11,49 +11,78 @@ new Swiper('.swiper.events__slider', {
     },
 
     breakpoints: {
-      320: {
-        slidesPerView: 1.5,
-        spaceBetween: 10,
-      },
-      575: {
-        slidesPerView: 2.5,
-        spaceBetween: 10,
-      },
-      1024: {
-        slidesPerView: 3.5,
-        spaceBetween: 10,
-      },
-      1440: {
-        slidesPerView: 4.5,
-        spaceBetween: 20,
-      }
-  },
+        320: {
+            slidesPerView: 1.5,
+            spaceBetween: 10,
+        },
+        575: {
+            slidesPerView: 2.5,
+            spaceBetween: 10,
+        },
+        1024: {
+            slidesPerView: 3.5,
+            spaceBetween: 10,
+        },
+        1440: {
+            slidesPerView: 4.5,
+            spaceBetween: 20,
+        }
+    },
 });
 
 new Swiper('.swiper.gallery__slider', {
-  slidesPerView: 3.2,
-  centeredSlides: true,
-  spaceBetween: 45,
-  loop: true,
-  speed: 500,
+    slidesPerView: 3.2,
+    centeredSlides: true,
+    spaceBetween: 45,
+    loop: true,
+    speed: 500,
 
-  navigation: {
-    nextEl: '.gallery .gallery__slider-btns.swiper-button-next',
-    prevEl: '.gallery .gallery__slider-btns.swiper-button-prev',
-  },
+    navigation: {
+        nextEl: '.gallery .gallery__slider-btns.swiper-button-next',
+        prevEl: '.gallery .gallery__slider-btns.swiper-button-prev',
+    },
 
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 10,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 25,
+        },
+        1440: {
+            slidesPerView: 3.2,
+            spaceBetween: 45,
+        }
     },
-    1024: {
-      slidesPerView: 2,
-      spaceBetween: 25,
+});
+
+new Swiper('.swiper.shop__slider', {
+    slidesPerView: 4.5,
+    spaceBetween: 20,
+
+    navigation: {
+        nextEl: '.shop .shop__slider-btns.swiper-button-next',
+        prevEl: '.shop .shop__slider-btns.swiper-button-prev',
     },
-    1440: {
-      slidesPerView: 3.2,
-      spaceBetween: 45,
-    }
-},
+
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        575: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        1440: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        }
+    },
 });
